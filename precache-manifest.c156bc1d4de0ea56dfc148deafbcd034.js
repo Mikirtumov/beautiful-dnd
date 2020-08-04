1dfdd132ccd3894c40af1976f269b7492e12cd8f
Sergo Mikirtumov <mikirtumov@gmail.com>
2020-08-04 01:15:20 -0700

    Updates

---
+++ precache-manifest.c156bc1d4de0ea56dfc148deafbcd034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ead56ab7ec246eda9ac0838385d449",
+    "url": "/beautiful-dnd/index.html"
+  },
+  {
+    "revision": "930beafa947df04fd62d",
+    "url": "/beautiful-dnd/static/js/2.8bef7b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beautiful-dnd/static/js/2.8bef7b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40eaa2697e5c53afc733",
+    "url": "/beautiful-dnd/static/js/main.3ef60043.chunk.js"
+  },
+  {
+    "revision": "6a5b790358b81bc1b40f",
+    "url": "/beautiful-dnd/static/js/runtime-main.057833cb.js"
+  }
+]);
---
+++ static/js/main.3ef60043.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbeautiful-dnd"]=this["webpackJsonpbeautiful-dnd"]||[]).push([[0],[,,,function(e,n,t){e.exports=t(4)},function(e,n,t){"use strict";t.r(n);var u=t(0),o=t.n(u),r=t(2),l=function(){return"Hello World"};t.n(r).a.render(o.a.createElement(l,null),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.3ef60043.chunk.js.map
